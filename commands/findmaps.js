e560710ea1d0fd43a5223ee1c99f55aaa3877136
spikegd <spikegdofficial@gmail.com>
2020-12-06 21:31:58 -0800

    Add map search

---
+++ commands/findmaps.js
@@ -0,0 +1,25 @@
+const { find } = require('beatsaver')
+const { MessageEmbed } = require('discord.js')
+
+module.exports.run = async (bot, message, args) => {
+  const query = args[1]
+  
+  if (!query) return message.channel.send('Please provide a query to search for maps with!')
+
+  const maps = await find(query)
+  const embed = new MessageEmbed()
+    .setTitle('Map Results for "' + query + '"')
+    .setColor('GREEN')
+
+  for (let i = 0; i < 6; i++) {
+    const m = maps.docs[i]
+    
+    if (m) {
+      embed.addField(m.name,
+        `Downloads: ${m.stats.downloads.toLocaleString()}\nAuthor: ${m.metadata.levelAuthorName}\nLink: https://beatsaver.com/beatmap/${m.key}\nUploaded on: ${m.uploaded.split('T')[0]}`,
+        true)
+    }
+  }
+
+  message.channel.send(embed)
+}
---
+++ commands/getmap.js
@@ -0,0 +1,6 @@
+const { findMap } = require('../functions/util')
+const { MessageEmbed } = require('discord.js')
+
+module.exports.run = async (bot, message, args) => {
+  
+}
